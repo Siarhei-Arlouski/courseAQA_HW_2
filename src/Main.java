1d0c7c469b0cd400d34ae47219c677117cbf44f9
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-10-21 11:40:38 +0300

    Added variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Сергей Орловский, QA");
+        System.out.println("Siarhei Arlouski, QA");
+    variables();
+    }
+    public static void variables(){
+        byte varByte = 12;
+        System.out.println("byte: " + varByte);
+        short varShort = 3245;
+        System.out.println("short: " + varShort);
+        int vatInt = 41251553;
+        System.out.println("int: " + vatInt);
+        long varLong = 75463548523523528L;
+        System.out.println("long: " + varLong);
+        float varFloat = 5.12F;
+        System.out.println("float: " + varFloat);
+        double varDouble = 3.6234543624D;
+        System.out.println("double: " + varDouble);
+        boolean varBoolean = true;
+        System.out.println("true: " + varBoolean);
+        char varChar = 'A';
+        System.out.println("char: " + varChar);
     }
 }
