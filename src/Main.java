4ea2e627ced0a337631e6311e3a230d80fdc21e3
Siarhei Arlouski <siarhei.arlouski@gmail.com>
2022-10-21 12:20:28 +0300

    Added strings scanner

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Siarhei Arlouski, QA");
     variables();
+    System.out.println("Enter the text:");
+    stringScanner();
     }
     public static void variables(){
         byte varByte = 12;
@@ -20,5 +24,27 @@ public class Main {
         System.out.println("true: " + varBoolean);
         char varChar = 'A';
         System.out.println("char: " + varChar);
+    }
+    public static void stringScanner(){
+        Scanner stringScanner = new Scanner(System.in);
+        String enterTheText = stringScanner.nextLine();
+        int stringLength = enterTheText.length();
+        System.out.println("length: " + stringLength);
+        boolean stringIsEmpty = enterTheText.isEmpty();
+        System.out.println("isEmpty: " + stringIsEmpty);
+        char stringCharAt = enterTheText.charAt(7);
+        System.out.println("charAt(7): " + stringCharAt);
+        boolean stringStartsWith = enterTheText.startsWith("This");
+        System.out.println("startsWith('This'): " + stringStartsWith);
+        boolean stringEndsWith = enterTheText.endsWith("mine");
+        System.out.println("endsWith('mine'): " + stringEndsWith);
+        boolean stringContains = enterTheText.contains("is");
+        System.out.println("contains('is'): " + stringContains);
+        String stringConcat = enterTheText.concat(". The end");
+        System.out.println("concat: " + stringConcat);
+        String stringReplace = enterTheText.replace('i', 'a');
+        System.out.println("replace: " + stringReplace);
+        String stringToLowerCase = enterTheText.toLowerCase();
+        System.out.println("toLowerCase: " + stringToLowerCase);
     }
 }
